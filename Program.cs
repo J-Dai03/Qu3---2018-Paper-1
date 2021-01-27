bfc5323de2f3a4442d7864ea3ee6cd56a03d7687
James Dai <K038246@eltham-college.org.uk>
2021-01-27 11:33:06 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Qu3___2018_Paper_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = 0;
+            int c;
+            while ((Number < 1) || (Number > 10))
+            {
+                Console.WriteLine("Enter a positive whole number: ");
+                Number = Convert.ToInt32(Console.ReadLine());
+                if (Number > 10)
+                {
+                    Console.WriteLine("Number too large.");
+                }
+                else if(Number < 1)
+                {
+                    Console.WriteLine("Not a positive number.");
+                }
+                else
+                {
+                    //Do nothing
+                }
+            }
+            //End while
+            c = 1;
+            //Start for
+            for (int k = 0; k < Number; k++)
+            {
+                Console.WriteLine(Convert.ToString(c));
+                c = (c * (Number - 1 - k)) / (k + 1);
+            }
+        }
+    }
+}
